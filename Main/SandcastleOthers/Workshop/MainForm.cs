2333c424cd4ac630feab11af766a7b9554eebd50
Paul Selormey <809351+paulushub@users.noreply.github.com>
2008-10-02 14:55:29 +0000

    Created a new Workshop project, under the SandcastleOthers solutions.

---
+++ Main/SandcastleOthers/Workshop/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Collections.Generic;
+
+namespace Sandcastle.Workshop
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
